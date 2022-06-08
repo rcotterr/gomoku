bc69a4bbe53e82dcd86e63688760e05fe413e13b
marina.romashkova <Marina.Romashkova@kaspersky.com>
2022-06-08 19:21:09 +0300

    gomoku

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const n = 19
+
+func printPlayBoard(playBoard string) {
+	fmt.Println("current play board:")
+	for i, val := range playBoard {
+		if  i % n == 0 {
+			fmt.Println()
+		} else {
+			fmt.Print(string(val), "  ")
+		}
+	}
+	fmt.Println()
+}
 
 func main() {
 	fmt.Println("it's gomoku, let's play")
+	var startArray = strings.Repeat(".", n*n)
+	printPlayBoard(startArray)
 }
