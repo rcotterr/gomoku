60f3ccb7373690f0157b96be86f0f3c87f8c8729
marina.romashkova <Marina.Romashkova@kaspersky.com>
2022-06-12 19:26:32 +0300

    gomoku

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,8 @@ import (
 const n = 19
 const emptySymbol = "."
 const lenPositions = 2
-const player1 = 0
-const player2 = 1
+const player1 = "0"
+const player2 = "1"
 
 type Pos struct {
 	x int
@@ -53,7 +53,7 @@ func parsePositions(text string) (*Pos, error) {
 	return &pos, nil
 }
 
-func putStone(playBoard string, pos *Pos, currentPlayer int) (string, error) {
+func putStone(playBoard string, pos *Pos, currentPlayer string) (string, error) {
 
 	index := pos.y * n + pos.x
 	fmt.Println(index)
@@ -61,9 +61,85 @@ func putStone(playBoard string, pos *Pos, currentPlayer int) (string, error) {
 		return "", fmt.Errorf("position is busy")
 	}
 
-	newPlayBoard := strings.Join([]string{playBoard[:index], strconv.FormatInt(int64(currentPlayer), 10), playBoard[index+1:]}, "")
+	newPlayBoard := strings.Join([]string{playBoard[:index], currentPlayer, playBoard[index+1:]}, "")
 
 	return newPlayBoard, nil
+}
+
+func checkFive(playBoard string, i int, symbol string) bool {
+	count := 1 //TO DO add if i % n + 5 >= n
+	j := i + 1
+	for j % n != 0 {
+		if string(playBoard[j]) == symbol {
+			count += 1
+		} else {
+			break
+		}
+		j += 1
+	}
+	if count >= 5 {
+		fmt.Println("Game is over, CONGRATULATIONS TO PLAYER ", symbol)
+		return true
+	}
+
+	count = 1
+	j = i + n
+	for j / n != n {
+		if string(playBoard[j]) == symbol {
+			count += 1
+		} else {
+			break
+		}
+		j += n
+	}
+	if count >= 5 {
+		fmt.Println("Game is over, CONGRATULATIONS TO PLAYER ", symbol)
+		return true
+	}
+
+	count = 1
+	j = i + n + 1
+	for j < n*n {
+		if string(playBoard[j]) == symbol {
+			count += 1
+		} else {
+			break
+		}
+		j += n + 1
+	}
+	if count >= 5 {
+		fmt.Println("Game is over, CONGRATULATIONS TO PLAYER ", symbol)
+		return true
+	}
+
+	return false
+	//TO DO add possibleCapture
+	// 6 stones and capture only in 6
+	// test not sequence
+}
+
+func isOver(playBoard string) bool {
+	for i, val := range playBoard {
+		if string(val) == player1 {
+			if checkFive(playBoard, i, player1) {
+				return true
+			}
+			i += 1
+		}
+		if string(val) == player2 {
+			if checkFive(playBoard, i, player2) {
+				return true
+			}
+			i += 1
+		}
+		if string(val) == emptySymbol {
+			i += 1
+		}
+	}
+
+	//TO DO add 10 captured
+	//add no space left
+	return false
 }
 
 func main() {
@@ -73,7 +149,7 @@ func main() {
 	currentPlayer := player1
 	anotherPlayer := player2
 	reader := bufio.NewReader(os.Stdin)
-	for true { //TODO for not over
+	for !isOver(playBoard) {
 		fmt.Println("Player ", currentPlayer, ", enter positions (like 1 2):")
 		text, _ := reader.ReadString('\n')
 		pos, err := parsePositions(text)
@@ -91,3 +167,10 @@ func main() {
 		currentPlayer, anotherPlayer = anotherPlayer, currentPlayer
 	}
 }
+
+
+//Bonuses
+//- players' names
+//- tests
+//
+//
