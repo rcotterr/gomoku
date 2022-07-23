be4c85a3508bb36f57c41b2a2dd186b1f0d34b50
marina.romashkova <Marina.Romashkova@kaspersky.com>
2022-07-23 19:52:19 +0300

    gomoku

--- pkg/visualize/visualize.go
+++ pkg/visualize/visualize.go
@@ -1,10 +1,12 @@
 package visualize
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"image/color"
 	_ "image/jpeg"
 	"log"
+	"time"
 )
 import (
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,22 +14,60 @@ import (
 	//"github.com/hajimehoshi/ebiten/v2/inpututil"
 	//"bytes"
 )
+import _ "image/png"
 
-type Game struct{}
+type Game struct {
+	screen *ebiten.Image
+}
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
-	//screenWidth  = 1600
-	//screenHeight = 1040
+	//screenWidth  = 640
+	//screenHeight = 480
+	screenWidth  = 1280
+	screenHeight = 720
 )
 
-var image = ebiten.NewImage(screenWidth, screenHeight)
+//func init() {
+//WhiteStone, _, err := ebitenutil.NewImageFromFile("img/whiteStone.jpg")
+//_, _, err = ebitenutil.NewImageFromFile("img/blackStone.jpg")
+//if err != nil {
+//	log.Fatal(err)
+//}
+//}
+
+var _ = ebiten.NewImage(screenWidth, screenHeight)
+
+//var WhiteStone, _, err_ = ebitenutil.NewImageFromFile("img/whiteStone_copy.png")
+
+var WhiteStone *ebiten.Image
+
+func init() {
+	var err error
+	//WhiteStone, _, err = ebitenutil.NewImageFromFile("img/whiteStone.jpg")
+	//WhiteStone, _, err = ebitenutil.NewImageFromFile("img/whiteStone_.png")
+	//WhiteStone, _, err = ebitenutil.NewImageFromFile("img/whiteStone_copy.png")
+	//WhiteStone, _, err = ebitenutil.NewImageFromFile("img/whiteStone_copy_8.png")
+	WhiteStone, _, err = ebitenutil.NewImageFromFile("img/WhiteStone.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//var WhiteStone, _, _ = ebitenutil.NewImageFromFile("img/whiteStone.jpg")
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
 	// Write your game's logical update.
+	//mx, my := ebiten.CursorPosition()
+	////fmt.Println(mx, my)
+	//if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+	//	op := &ebiten.DrawImageOptions{}
+	//	op.GeoM.Translate(float64(mx), float64(my))
+	//	screen.DrawImage(WhiteStone, op)
+	//}
+	fmt.Println("test update", time.Now())
+
 	return nil
 }
 
@@ -36,6 +76,9 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.NRGBA{R: 222, G: 184, B: 135, A: 255})
 	gridColor64 := &color.RGBA{A: 50}
+	//if WhiteStone == nil {
+	//	fmt.Println(err_)
+	//}
 	//gridColor32 := &color.RGBA{A: 20}
 	//const w = screenWidth
 	//const h = screenHeight
@@ -52,9 +95,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	//	ebitenutil.DrawLine(screen, x, 0, x, h, gridColor64)
 	//}
 
-	width := 10
+	width := 12
 	lines := 19
-	start := 40
+	start := 10
 	//plus := 32
 	xStart, yStart := start, start
 	xEnd, yEnd := xStart+width*(lines-1), yStart+width*(lines-1)
@@ -82,23 +125,32 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	//
 	//pygame.display.update()
 
-	//screen.DrawImage(image, nil)
+	//screen.DrawImage(WhiteStone, nil)
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(10), float64(10))
+	//op.ColorM.Scale(1.0, 0.25, 0.25, 1.0)
+	//theta := 2.0 * math.Pi * float64(count%60) / 60.0
+	//op.ColorM.Concat(ebiten.RotateHue(theta))
+	screen.DrawImage(WhiteStone, op)
+
+	//mx, my := ebiten.CursorPosition()
+	////fmt.Println(mx, my)
+	//if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+	//	op = &ebiten.DrawImageOptions{}
+	//	op.GeoM.Translate(float64(mx), float64(my))
+	//	screen.DrawImage(WhiteStone, op)
+	//}
 	// Write your game's rendering.
+
+	fmt.Println("test draw", time.Now())
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 360
 	//return screenWidth/2, screenHeight/2
-}
-
-func init() {
-	_, _, err := ebitenutil.NewImageFromFile("img/whiteStone.jpg")
-	_, _, err = ebitenutil.NewImageFromFile("img/blackStone.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func (g *Game) Draw_(screen *ebiten.Image) {
@@ -155,5 +207,10 @@ func Vis() {
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
+	//mx, my := ebiten.CursorPosition()
+	//fmt.Println(mx, my)
+	//if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+	//
+	//}
 
 }
