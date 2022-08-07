9b4c0d89cab1af93758873ac298f30f60ff5f85e
Amory lorch Romny weaver <aromny-w@oa-g3.msk.21-school.ru>
2022-08-07 23:18:14 +0300

    gomoku

--- pkg/playboard/algo.go
+++ pkg/playboard/algo.go
@@ -50,12 +50,9 @@ func Heuristic(state State, symbol string, captures int) float64 {
 	for step, condition := range setRules {
 		count, halfFree, free, _ := CountInRow(state.Node, state.index, step, condition, symbol)
 		if count >= 5 || captures >= 5 { // TO DO and not capture
-			//num = 1000000000
-			//break
 			return math.Inf(1)
 		} else if count == 4 && free {
-			//num += 10000000
-			return math.Inf(1)
+			num += 1000000
 		} else if count == 4 && halfFree {
 			num += 100000
 		} else if count == 3 && free { //TO DO free more than one step
@@ -74,9 +71,9 @@ func Heuristic(state State, symbol string, captures int) float64 {
 
 	}
 
-	num += float64(1000000 * state.Captures)
+	num += float64(100000 * state.Captures)
 	if vulnerable == true {
-		num -= 1000000
+		num -= 100000
 	}
 
 	return num
@@ -149,29 +146,42 @@ func getIndexes(children []Child) intSet {
 }
 
 type Child struct {
+	h1    float64
+	h2    float64
 	Value float64
 	State State
 }
 
 const numChildren = 3
 
-func getHeuristic(state State, captures int, multiplier int) float64 {
+func getHeuristic(state State, player Player, opponent Player, multiplier int) (float64, float64, float64) {
 	var h1, h2 float64
 
-	if string(state.Node[state.index]) == string('M') {
-		h1 = Heuristic(state, string('M'), captures)
-		state.Node = strings.Join([]string{state.Node[:state.index], string('0'), state.Node[state.index+1:]}, "")
-		h2 = Heuristic(state, string('0'), captures)
+	h1 = Heuristic(state, player.Symbol, player.Captures)
+	//state.Node = strings.Join([]string{state.Node[:state.index], opponent.Symbol, state.Node[state.index+1:]}, "")
+	state.Node = strings.Join([]string{state.Node[:state.index], EmptySymbol, state.Node[state.index+1:]}, "")
+	state, err := PutStone(state.Node, state.index, &opponent)
+	if err == nil {
+		h2 = Heuristic(state, opponent.Symbol, opponent.Captures)
 	} else {
-		h2 = Heuristic(state, string('0'), captures)
-		state.Node = strings.Join([]string{state.Node[:state.index], string('M'), state.Node[state.index+1:]}, "")
-		h1 = Heuristic(state, string('M'), captures)
+		h2 = 0
 	}
 
-	return float64(multiplier) * (h1 + h2)
+	return h1, h2, float64(multiplier) * (h1 + h2)
+	//if string(state.Node[state.index]) == string('M') {
+	//	h1 = Heuristic(state, string('M'), player.Captures)
+	//	state.Node = strings.Join([]string{state.Node[:state.index], string('0'), state.Node[state.index+1:]}, "")
+	//	h2 = Heuristic(state, string('0'), player.Captures)
+	//} else {
+	//	h2 = Heuristic(state, string('0'), player.Captures)
+	//	state.Node = strings.Join([]string{state.Node[:state.index], string('M'), state.Node[state.index+1:]}, "")
+	//	h1 = Heuristic(state, string('M'), player.Captures)
+	//}
+	//
+	//return float64(multiplier) * (h1 + h2)
 }
 
-func cutChildren(children []State, transpositions stringSet, captures int, multiplier int) []Child {
+func cutChildren(children []State, transpositions stringSet, player Player, opponent Player, multiplier int) []Child {
 
 	var new_ []Child
 	for _, childState := range children {
@@ -181,18 +191,23 @@ func cutChildren(children []State, transpositions stringSet, captures int, multi
 			continue
 		}
 		transpositions[childState.Node] = member
-		h := -getHeuristic(State{childState.Node, childState.index, childState.Captures}, captures, -multiplier)
-		new_ = append(new_, Child{h, State{childState.Node, childState.index, childState.Captures}})
-
+		h1, h2, value := getHeuristic(State{childState.Node, childState.index, childState.Captures}, player, opponent, -multiplier)
+		new_ = append(new_, Child{h1, h2, -value, State{childState.Node, childState.index, childState.Captures}})
 	}
 
 	sort.Slice(new_, func(i, j int) bool {
+		if new_[i].Value == new_[j].Value {
+			return new_[i].h1 > new_[j].h1
+		}
+
 		return new_[i].Value > new_[j].Value
 	})
 
 	for i := range children {
 		if i+1 < len(new_) {
 			if new_[i].Value > new_[i+1].Value {
+				return new_[:i+1]
+			} else if new_[i].h1 > new_[i+1].h1 {
 				return new_[:i+1]
 			}
 		} else {
@@ -306,10 +321,10 @@ func NegaScout(state State, depth int, alpha float64, beta float64, multiplier i
 
 	if multiplier == 1 {
 		children = getChildren(state.Node, state.index, machinePlayer, childIndexesSet)
-		childrenSlice = cutChildren(children, transpositions, machinePlayer.Captures, multiplier)
+		childrenSlice = cutChildren(children, transpositions, machinePlayer, humanPlayer, multiplier)
 	} else {
 		children = getChildren(state.Node, state.index, humanPlayer, childIndexesSet)
-		childrenSlice = cutChildren(children, transpositions, humanPlayer.Captures, multiplier)
+		childrenSlice = cutChildren(children, transpositions, humanPlayer, machinePlayer, multiplier)
 	}
 	//childrenSlice = cutChildren(children, transpositions, multiplier)
 
